ce6615f919043140d143a4c732daa9b5f13dff16
lizwachira <lizwanguwachira@gmail.com>
2024-04-09 00:34:21 +0300

    add event listener

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+
+function addEventListener() {
+
+const input = document.getElementById('button')
+input.addEventListener('click',function(){
+    alert('I was clicked!') //a pop-up window shows up
+    
+})
 }
+
